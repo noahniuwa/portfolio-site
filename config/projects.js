550a250ed5a4d5e2b78850d17c720b4b41c2dc22
Noah Maizels <noahmaizels@hotmail.com>
2020-03-08 23:24:04 +0800

    changed text

--- config/projects.js
+++ config/projects.js
@@ -4,7 +4,7 @@ const projects = {
   noteKeeper: {img: 'notekeeper.png', description: 'Note Keeper - Next.js, ReactBeautifulDND', site: 'https://note-keeper.now.sh', code: 'https://github.com/noahniuwa/ListKeeper'},
   weather: { img: 'weather.png', description: 'Weather APP - React', site: 'https://xenodochial-wozniak-0d64b8.netlify.com', code: 'https://github.com/noahniuwa/react-practice-app' },
   pricePage: { img: 'pricepage.png', description: 'Product Price Page - HTML/CSS', site: 'https://noahniuwa.github.io/price-page/', code: 'https://github.com/noahniuwa/price-page' },
-  rockPaperScissors: { img: 'rockpaperscissors.png', description: 'Rock, Paper, Scissors Game - Create React App', site: 'https://rock-paper-scissors-143.netlify.com', code: 'https://github.com/noahniuwa/rock-paper-scissors-new' },
+  rockPaperScissors: { img: 'rockpaperscissors.png', description: 'Rock, Paper, Scissors - React', site: 'https://rock-paper-scissors-143.netlify.com', code: 'https://github.com/noahniuwa/rock-paper-scissors-new' },
   exploreWanchain: { img: 'explorewanchain.png', description: 'Docs site for Wanchain - Docsify', site: 'https://www.explorewanchain.org/#/', code: '' },
   finnexusDocs: { img: 'finnexusdocs.png', description: 'Docs site for FinNexus - Docsify', site: 'https://www.docs.finnexus.io', code: 'https://github.com/noahniuwa/FinNexus-Docs'},
   wanchainGuide: { img: 'wanchainguide.png', description: 'Wanchain wallet guide - React', site: 'https://wanchain.guide/#/', code: 'https://github.com/noahniuwa/wan-guide' },
